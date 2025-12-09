5290111842ec6373e01f589c34990d6911a28068
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-08 21:27:09 -0300

    comencando a criar a estrutura do projeto

---
+++ bin/notas.dart
@@ -0,0 +1,9 @@
+void main() {
+
+}
+
+String getComando() {
+  print("Digite um Comando: 1 - Adicionar nota, 2 - Listar notas, 3 - Sair");
+  List<String> comando =  <String> ["1, 2, 3,"];
+  String? estrada = "";
+}
